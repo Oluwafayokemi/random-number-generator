cfa40c680e9ed992507c8ab00b57cf092a7b55ff
Fayoke Adeyina <fayokeadeyina@Fayokes-MacBook-Pro.local>
2019-04-08 17:20:13 +0100

    set up create-react-app

---
+++ generate-random-number/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import RandomNumber from './components/randomNumber/randomNumber';
+
+const App = () =>(
+  <RandomNumber />
+)
+export default App;
---
+++ generate-random-number/src/components/randomNumber/randomNumber.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const RandomNumber = () => (
+    <h1>I am a random Number</h1>
+)
+export default RandomNumber
